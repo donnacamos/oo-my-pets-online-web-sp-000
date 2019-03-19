45b8809aa3976e8384c533a9a8129d5c1ea606a4
Donna Amos <donnacamos.developer@gmail.com>
2019-03-19 20:48:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,17 +30,20 @@ def walk_dogs
   @pets[:dogs].each do |dog| 
     dog.mood = "happy"
   end 
-  
+end 
+
   def play_with_cats 
     @pets[:cats].each do |cat| 
       cat.mood = "happy" 
     end 
-    
+  end 
+  
     def feed_fish 
       @pets[:fishes].each do |fish|
         fish.mood = "happy" 
       end 
-      
+  end 
+  
     def sell_pets
       @pets.each do |type, name_array| 
         name_array.each do |pet| 
